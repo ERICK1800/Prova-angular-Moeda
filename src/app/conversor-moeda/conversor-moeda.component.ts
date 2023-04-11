58f9558acc89d4ed8b585f9cac03dc86b07fb982
ERICK1800 <ericklimaflorencio@gmail.com>
2023-04-11 09:05:07 -0300

    commit 2

--- src/app/conversor-moeda/conversor-moeda.component.ts
+++ src/app/conversor-moeda/conversor-moeda.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./conversor-moeda.component.css']
 })
 export class ConversorMoedaComponent {
+  num1: number;
+  moeda1: string;
+  num2: number;
+  moeda2: string;
+  resp: number;
 
+  converter(){
+
+    switch (key) {
+      case value:
+        
+        break;
+    
+      default:
+        break;
+    }
+
+    if (this.moeda1 == "USD" && this.moeda2 == "EUR") {
+      
+    }
+    this.moeda1;
+    this.moeda2;
+    this.num1;
+    this.num2;
+    this.resp;
+  }
 }
