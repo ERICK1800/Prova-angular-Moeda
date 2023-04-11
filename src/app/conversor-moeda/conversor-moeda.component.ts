98f62b8761d29ff8dbf1929fda7c24fa50533e2a
ERICK1800 <ericklimaflorencio@gmail.com>
2023-04-11 10:21:15 -0300

    commit 5

--- src/app/conversor-moeda/conversor-moeda.component.ts
+++ src/app/conversor-moeda/conversor-moeda.component.ts
@@ -8,33 +8,37 @@ import { Component } from '@angular/core';
 export class ConversorMoedaComponent {
   num1: number = 0;
   num2: number = 0;
-  moeda :string = "0";
-  resp: number = 0;
+  moeda: string = "";
+  resp: string = "";
+  conv: string = "";
 
   converter(){
+
+    this.conv = (this.num1 * this.num2).toFixed(2);
+
     switch (this.moeda) {
       case "1":
-        this.resp = this.num1 * this.num2; // Conversão de dólar para euro
+        this.resp = 'A conversão de Dólar Americano (USD) para Euro (EUR) é ficou em: ' + this.conv;
         break;
 
       case "2":
-        this.resp = this.num1 * this.num2; // Conversão de dólar para real
+        this.resp = 'A conversão de Dólar Americano (USD) para Real Brasileiro (BRL) é ficou em: ' + this.conv;
         break;
 
       case "3":
-        this.resp = this.num1 * this.num2; // Conversão de euro para dólar
+        this.resp = 'A conversão de Euro (EUR) para Dólar Americano (USD) é ficou em: ' + this.conv;
         break;
 
       case "4":
-        this.resp = this.num1 * this.num2; // Conversão de euro para real
+        this.resp = 'A conversão de Euro (EUR) para Real Brasileiro (BRL) é ficou em: ' + this.conv;
         break;
 
       case "5":
-        this.resp =  this.num1 * this.num2; //Conversão de real para dólar
+        this.resp = 'A conversão de Real Brasileiro (BRL) para Dólar Americano (USD) é ficou em: ' + this.conv;
         break;
 
       case "6":
-        this.resp =  this.num1 * this.num2; // Conversão de real para euro
+        this.resp = 'A conversão de Real Brasileiro (BRL) para Euro (EUR) é ficou em: ' + this.conv;
         break;
     
       default:
